98cd579422fa46461318ba6871aa1f677783160f
TuanMageRO <162827395+TuanMageRO@users.noreply.github.com>
2024-09-11 09:29:38 +0700

    Add files via upload

---
+++ P0071_TaskManangement/src/Object/Task.java
@@ -0,0 +1,89 @@
+
+package Object;
+
+public class Task {
+    private int id;
+    private String taskTypeID, requirementName, date;
+    private String planFrom, planTo;
+    private String assignee, reviewer;
+
+    public Task() {
+    }
+
+    public Task(int id, String requirementName, String taskTypeID,  String date, String planFrom, String planTo, String assignee, String reviewer) {
+        this.id = id;
+        this.taskTypeID = taskTypeID;
+        this.requirementName = requirementName;
+        this.date = date;
+        this.planFrom = planFrom;
+        this.planTo = planTo;
+        this.assignee = assignee;
+        this.reviewer = reviewer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTaskTypeID() {
+        return taskTypeID;
+    }
+
+    public void setTaskTypeID(String taskTypeID) {
+        this.taskTypeID = taskTypeID;
+    }
+
+    public String getRequirementName() {
+        return requirementName;
+    }
+
+    public void setRequirementName(String requirementName) {
+        this.requirementName = requirementName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getPlanFrom() {
+        return planFrom;
+    }
+
+    public void setPlanFrom(String planFrom) {
+        this.planFrom = planFrom;
+    }
+
+    public String getPlanTo() {
+        return planTo;
+    }
+
+    public void setPlanTo(String planTo) {
+        this.planTo = planTo;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public String getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(String reviewer) {
+        this.reviewer = reviewer;
+    }
+    
+    
+}
